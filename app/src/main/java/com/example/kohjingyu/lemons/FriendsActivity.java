d06ffa3c55d03975420ce0f1e9aa7d56cc78d7ca
Vincentcent1 <Vincent@mymail.sutd.edu.sg>
2017-11-18 22:00:25 +0800

    added search part 1

--- app/src/main/java/com/example/kohjingyu/lemons/FriendsActivity.java
+++ app/src/main/java/com/example/kohjingyu/lemons/FriendsActivity.java
@@ -1,7 +1,11 @@
 package com.example.kohjingyu.lemons;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.SearchManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.SearchView;
 
 public class FriendsActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class FriendsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
+
+        //Get SearchView and set searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) findViewById(R.id.friends_searchView);
+
+        //Set the searchview searchable
+        searchView  .setSearchableInfo(searchManager
+                    .getSearchableInfo(new ComponentName(this, SearchableActivity.class)));
+//        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
     }
 }
